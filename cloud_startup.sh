faf844021e5a7a6211b8591fe829a5c1439cd18d
Fotoblysk <fotoblysk@fejm.pl>
2024-08-02 17:34:50 +0200

    orch test

---
+++ cloud_startup.sh
@@ -0,0 +1,7 @@
+git clone https://raw.githubusercontent.com/Fotoblysk/avg-transfer-rl/
+cd avg-transfer-rl
+git checkout cloud_sync
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+./bash_runnup
